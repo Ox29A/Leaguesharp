41efb320d6f7b19232490d0dd699c5a7ae53efd1
iJabbaReborn <iJabba@outlook.com>
2016-06-03 23:08:11 +0100

    FFF

--- iTwitch/iTwitch/Extensions.cs
+++ iTwitch/iTwitch/Extensions.cs
@@ -21,7 +21,7 @@
 
         public static float GetPoisonDamage(Obj_AI_Base target)
         {
-            if (target == null || !target.HasBuff("twitchdeadlyvenom") || target.IsInvulnerable || target.HasBuff("KindredRNoDeathBuff"))
+            if (target == null || !target.HasBuff("twitchdeadlyvenom") || target.IsInvulnerable || target.HasBuff("KindredRNoDeathBuff") || target.HasBuffOfType(BuffType.SpellShield))
             {
                 return 0;
             }
